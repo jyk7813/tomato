56c29901de2533db48226e90e41c1ef8d4da6041
juhatae <juhatae@192.168.50.24>
2023-06-22 17:13:10 +0900

    라이브러리 추가 확인 부탁

--- tomatoPj/src/tomatoPj/first.java
+++ tomatoPj/src/tomatoPj/first.java
@@ -1,9 +1,39 @@
 package tomatoPj;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dbutil.DBUtil;
+
 public class first {
 
 	public static void main(String[] args) {
 		System.out.println("hello tomato!");
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DBUtil.getConnection();
+			stmt= conn.prepareStatement("SELECT * FROM books");
+			rs = stmt.executeQuery();
+			
+			while (rs.next()) {
+				System.out.println(rs.getObject(2));
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			DBUtil.close(rs);
+			DBUtil.close(stmt);
+			DBUtil.close(conn);
+		}
+		
 	}
 
 }
