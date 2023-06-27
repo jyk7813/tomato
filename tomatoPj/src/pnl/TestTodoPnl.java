98ceb35cec9dd800f956fc2b2377f56afdbc0a98
yxxn.dev <yxxn.dev@gmail.com>
2023-06-27 14:18:03 +0900

    Utility 클래스에 버튼 객체 생성 관련 메소드 추가, todo페이지 테스트용 이미지 일부 추가

--- tomatoPj/src/pnl/TestTodoPnl.java
+++ tomatoPj/src/pnl/TestTodoPnl.java
@@ -1,107 +1,142 @@
 package pnl;
 
 import java.awt.Graphics;
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
-import javax.swing.ButtonGroup;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JToggleButton;
 
 import utility.IconData;
 import utility.Utility;
-
-public class TestTodoPnl extends JFrame implements ActionListener {
-	private IconData icon = new IconData();
-	private JPanel fixedTopPnl, topPnl, calendarPnl, monthSelPnl, projectSelPnl, checkSelPnl;
-	private JToggleButton totalTogle, projectTogle; // 전체 프로젝트 확인용 토글버튼, 프로젝트별 확인용 토글버튼
-	private ButtonGroup tg = new ButtonGroup(); // 토글 관리 그룹 객체
-	private static JButton kanbanMenu,todoMenu, plannerMenu;
+ 
+public class TestTodoPnl extends JFrame{
+	static final IconData ICON = new IconData();
+	static final Utility UT = new Utility();
 	
-	
-	public TestTodoPnl(){
-		
-		totalTogle = new JToggleButton(); // 전체 프로젝트 토글버튼
-		projectTogle = new JToggleButton(); // 프로젝트 별 토글버튼
-		fixedTopPnl = fixedTopPnl(); // 최상단 페이지 간 이동 버튼 고정 패널
-		topPnl = new JPanel(); // 월, 토글, 프로젝트 표시될 위치의 패널
-		calendarPnl = new JPanel(); // 달력 + 우측 태스크 정보 표시 패널
-		monthSelPnl = new JPanel(); // 월 선택 시 나타낼 패널
-		projectSelPnl = new JPanel(); // 프로젝트 선택 시 나타낼 패널
-		checkSelPnl = new JPanel(); // 햄버거 메뉴 선택 시 나타낼 패널
-		
-		// 토글버튼 그룹에 추가
-		tg.add(totalTogle);
-		tg.add(projectTogle);
-		
-		topPnl.add(totalTogle);
-		topPnl.add(projectTogle);
-
-		// 배경이미지 패널
-		BgPanel bgPnl = new BgPanel();
-		bgPnl.setLayout(null);
-		bgPnl.add(fixedTopPnl);
-		bgPnl.add(topPnl);
-		bgPnl.add(calendarPnl);
-		bgPnl.add(monthSelPnl);
-		bgPnl.add(projectSelPnl);
-		bgPnl.add(checkSelPnl);
-		this.add(bgPnl);
-		
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setExtendedState(JFrame.MAXIMIZED_BOTH);
-//		setUndecorated(true);
-		setVisible(true);
-	}
-	
-	JPanel fixedTopPnl(){
-		JPanel p = new JPanel();
-		kanbanMenu = new JButton(icon.kanbanBtnIcon());
-		todoMenu = new JButton(icon.todoBtnIcon());
-		plannerMenu = new JButton(icon.plannerBtnIcon());
-		
-		kanbanMenu.setBounds(400, 100, getWidth(), getHeight());
-		todoMenu.setBounds(600, 100, getWidth(), getHeight());
-		plannerMenu.setBounds(800, 100, getWidth(), getHeight());
-		
-		defaultSet(kanbanMenu);
-		defaultSet(todoMenu);
-		defaultSet(plannerMenu);
-		
-		return p;
-	}
-	
-	// JButton 기본 박스 아이콘을 투명하게 설정하는 메소드
-	JButton defaultSet(JButton btn) {
-		btn.setBorderPainted(false); // JButton의 외곽선 삭제
-		btn.setContentAreaFilled(false); // JButton의 내용 영역 채우기 안함 
-		btn.setFocusPainted(false); // JButton이 선택(focus)되었을 때 생기는 테두리 사용 안함
-		
-		return btn;
-	}
-	
-	
-	class BgPanel extends JPanel{
-		Image bg = new ImageIcon("img/todobg.jpg").getImage();
-		
-		public void paintComponent (Graphics g) {
-			g.drawImage(bg,  0,  0,  getWidth(), getHeight(), this);
+	// 최상단 고정 패널
+	static JPanel fixedTopPnl = new JPanel() {
+		Image background = ICON.getImageIcon("topPnlTestCom").getImage();
+		public void paint(Graphics g) {
+			g.drawImage(background, 0, 0, null);
+			super.paintChildren(g); // 패널 위 요소를 위에 다시 그리기
+			g.dispose();
 		}
+	};
+	
+	// 선택 패널
+	static JPanel selectPnl = new JPanel() {
+		Image background = ICON.getImageIcon("selectTest").getImage();
+		public void paint(Graphics g) {
+			g.drawImage(background, 0, 0, null);
+			super.paintChildren(g); // 패널 위 요소를 위에 다시 그리기
+			g.dispose();
+		}
+	};
+	
+	// 배경이미지 패널
+	static JPanel bgPnl = new JPanel() {
+		Image background = new ImageIcon("img/todobgTest.png").getImage();
+		public void paint(Graphics g) {//그리는 함수
+			g.drawImage(background, 0, 0, null);//background를 그려줌		
+		}
+	};
+	
+	public void homeframe() {
+		// -----------------------------------------------
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		setLayout(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		// -----------------------------------------------
+		// 최상단 고정 패널 추가
+		fixedTopPnl.setBounds(0, 0, getWidth(), getHeight());
+		fixedTopPnl.setLayout(null);
+		add(fixedTopPnl);
+		
+		// 선택 패널 추가
+		selectPnl.setBounds(0, 140, getWidth(), getHeight());
+		selectPnl.setLayout(null);
+		add(selectPnl);
+		
+		// 배경이미지 패널 추가
+		bgPnl.setLayout(null);
+		bgPnl.setBounds(0, 0, getWidth(), getHeight());
+		add(bgPnl);
+		// -----------------------------------------------
+		
+		
+		// 최상단 고정 패널
+		JButton kanbanMenu = UT.getBtn(650, 60, "navi_board2");
+		fixedTopPnl.add(kanbanMenu);
+		JButton todoMenu = UT.getBtn(870, 60, "navi_todo2");
+		fixedTopPnl.add(todoMenu);
+		JButton projectMenu = UT.getBtn(1060, 60, "navi_planner2");
+		fixedTopPnl.add(projectMenu);
+		
+		// 선택 패널
+		
+		
 	}
 	
-//	@Override
-//	public void actionPerformed(ActionEvent e) {
-//		// TODO Auto-generated method stub
-//		
+//	// JButton 기본 박스 아이콘을 투명하게 설정하는 메소드
+//	JButton defaultSet(JButton btn) {
+//		btn.setBorderPainted(false); // JButton의 외곽선 삭제
+//		btn.setContentAreaFilled(false); // JButton의 내용 영역 채우기 안함
+//		btn.setFocusPainted(false); // JButton이 선택(focus)되었을 때 생기는 테두리 사용 안함
+//
+//		return btn;
 //	}
 	
-	public static void main(String[] args) {
+	// 콤보박스
+//	JPanel comboPnl(String[] name) {
+//		JPanel pnl = new JPanel();
+//		JPanel top = new JPanel();
+//		List<String> contents = new ArrayList<>(Arrays.asList(name));
+//		String midName = "";
+//		
+//		for (int i = 0; i < contents.size(); i++) {
+//			JPanel 
+//		}
+//		JPanel mid = new JPanel();
+//		JPanel Bot = new JPanel();
+//		
+//		pnl.add(top);
+//		pnl.add(mid);
+//		pnl.add(bot);
+//		
+//		
+//		return pnl;
+//	}
+	
+	/**
+	 * JButton 객체 생성 후 반환해주는 메소드
+	 * 
+	 * @param int x(x좌표값), int y(y좌표값), String iconName(버튼에 설정할 아이콘 이미지 파일명)
+	 * @return JButton 
+	 */
+//	JButton getBtn(int x, int y, String iconName) {
+//		JButton btn = new JButton();
+//		btn.setIcon(ICON.getImageIcon(iconName));
+//		btn.setRolloverIcon(ICON.getRollImageIcon(iconName)); // 롤오버이미지 설정 (마우스 올렸을 때 이미지)
+//		btn.setSize(ICON.getImageIcon(iconName).getIconWidth(), ICON.getImageIcon(iconName).getIconHeight());
+//		btn.setBounds(x, y, btn.getWidth(), btn.getHeight());
+//		btn.setLayout(null);
+//		defaultSet(btn);
+//		return btn;
+//	}
+	
+	public TestTodoPnl() {
+		homeframe();
+	}
+	
+	
+	public static void main(String[] args){
 		new TestTodoPnl();
 	}
 }
--- tomatoPj/src/utility/IconData.java
+++ tomatoPj/src/utility/IconData.java
@@ -1,5 +1,6 @@
 package utility;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 public class IconData {
@@ -23,33 +24,12 @@ public class IconData {
 	public ImageIcon loginBtnIcon() {
 		return new ImageIcon(getClass().getClassLoader().getResource("login_btn.png"));
 	}
-
-	public ImageIcon kanbanBtnIcon() {
-		return new ImageIcon(getClass().getClassLoader().getResource("navi_board.png"));
-	}
-
-	public ImageIcon kanbanCBtnIcon() {
-		return new ImageIcon(getClass().getClassLoader().getResource("navi_board_c.png"));
-	}
-	
-	public ImageIcon todoBtnIcon() {
-		return new ImageIcon(getClass().getClassLoader().getResource("navi_todo.png"));
-	}
-
-	public ImageIcon todoCBtnIcon() {
-		return new ImageIcon(getClass().getClassLoader().getResource("navi_todo_c.png"));
-	}
-	
-	public ImageIcon plannerBtnIcon() {
-		return new ImageIcon(getClass().getClassLoader().getResource("navi_planner.png"));
-	}
-
-	public ImageIcon plannerCBtnIcon() {
-		return new ImageIcon(getClass().getClassLoader().getResource("navi_planner_c.png"));
-	}
 	
 	public ImageIcon getImageIcon(String iconName) {
 		return new ImageIcon(getClass().getClassLoader().getResource(iconName + ".png"));
 	}
-	
+
+	public ImageIcon getRollImageIcon(String iconName) {
+		return new ImageIcon(getClass().getClassLoader().getResource(iconName + "_c.png"));
+	}
 }
