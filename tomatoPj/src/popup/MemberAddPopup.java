72995ee2d7746e9821a903391cfccf10921d4c9d
jyk7813 <130989673+jyk7813@users.noreply.github.com>
2023-07-03 13:17:07 +0900

    addmember양식 완료

--- tomatoPj/src/popup/MemberAddPopup.java
+++ tomatoPj/src/popup/MemberAddPopup.java
@@ -1,46 +1,75 @@
 package popup;
 
-import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import button.AddMemberBtn;
 import frame.MainFrame;
+import utility.IconData;
+import utility.Utility;
 
-import java.awt.Color;
-import java.awt.event.*;
+public class MemberAddPopup extends JDialog {
+	JTextField idInsertTextField;
+	JButton addMemberBtn;
+	MainFrame mainFrame;
+	Utility utility;
+	private IconData iconData = new IconData();
 
-public class MemberAddPopup extends JDialog{
-    JTextField idInsertTextField;
-    JButton addMemberBtn;
-    MainFrame mainFrame;
-    
+	public MemberAddPopup() {
+		utility = new Utility();
+		ImageIcon imageIcon = iconData.getImageIcon("addmember(BG)");
 
-    public MemberAddPopup() {
-    	setLayout(null);
-        idInsertTextField = new JTextField();
-        idInsertTextField.setBounds(34, 57, 276, 41);
-        add(idInsertTextField);
-        addMemberBtn = new AddMemberBtn(mainFrame);
-        addMemberBtn.setBounds(110, 119, 126, 41);
-        add(addMemberBtn);
-        setSize(346,174);
-        setUndecorated(true);
-        setBackground(new Color(0,0,0,0));
-        setVisible(true);
-        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		JPanel panelWithBackground = new JPanel() {
+			@Override
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				// Draw the image on the panel
+				g.drawImage(imageIcon.getImage(), 0, 0, this.getWidth(), this.getHeight(), this);
+			}
+		};
+		// Optional: set the layout manager of the panel
+		panelWithBackground.setLayout(null);
 
-        // WindowFocusListener를 추가합니다.
-        this.addWindowFocusListener(new WindowFocusListener() {
-            @Override
-            public void windowGainedFocus(WindowEvent e) {
-                // 팝업 창이 포커스를 얻었을 때의 동작을 정의합니다.
-            }
+		// Add any other components to the panelWithBackground
+		panelWithBackground.setOpaque(false);
+		// Add the panel to the JDialog
+		this.getContentPane().add(panelWithBackground);
+		
+		idInsertTextField = new JTextField();
+		idInsertTextField.setBounds(34, 57, 276, 41);
+		utility.setButtonProperties(idInsertTextField);
+		panelWithBackground.add(idInsertTextField);
+		addMemberBtn = new AddMemberBtn(mainFrame);
+		addMemberBtn.setBounds(110, 119, 126, 41);
+		panelWithBackground.add(addMemberBtn);
+		setSize(346, 174);
+		setUndecorated(true);
+		setBackground(new Color(0, 0, 0, 0));
+		setVisible(true);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 
-            @Override
-            public void windowLostFocus(WindowEvent e) {
-                // 팝업 창이 포커스를 잃었을 때의 동작을 정의합니다.
-                // 여기서는 팝업 창을 닫습니다.
-                dispose();
-            }
-        });
-    }
+		// WindowFocusListener를 추가합니다.
+		this.addWindowFocusListener(new WindowFocusListener() {
+			@Override
+			public void windowGainedFocus(WindowEvent e) {
+				// 팝업 창이 포커스를 얻었을 때의 동작을 정의합니다.
+			}
+
+			@Override
+			public void windowLostFocus(WindowEvent e) {
+				// 팝업 창이 포커스를 잃었을 때의 동작을 정의합니다.
+				// 여기서는 팝업 창을 닫습니다.
+				dispose();
+			}
+		});
+	}
 }
