21d4b60c032b08fef6cc72d0277d111ab939df3e
jyk7813 <130989673+jyk7813@users.noreply.github.com>
2023-07-04 12:50:32 +0900

    머지?

--- tomatoPj/src/pnl/boradPnl/ColumnSelectPnl.java
+++ tomatoPj/src/pnl/boradPnl/ColumnSelectPnl.java
@@ -3,8 +3,6 @@ package pnl.boradPnl;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.SQLException;
